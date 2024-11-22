5664a410609cf2322f8cca76fcaf893405c57808
Sebastian Glahn <9576670+storatus@users.noreply.github.com>
2024-11-22 15:45:46 -0400

    More fixes (#2)
    * nicelyCreating PostForm
    
    * tried
    
    * and again
    
    * check

--- src/app/actions/blog.ts
+++ src/app/actions/blog.ts
@@ -1,3 +1,8 @@
+"use server";
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 export const getPosts = async () => {
   try {
     return [];
@@ -9,6 +14,26 @@ export const getPosts = async () => {
 export const getPost = async () => {
   try {
     return [];
+  } catch (error) {
+    console.error("Failed to fetch chat sessions", error);
+  }
+};
+
+interface CreatePost {
+  title: string;
+  content: string;
+}
+
+export const createPost = async ({ content, title }: CreatePost) => {
+  try {
+    const fetchedCreatePost = await prisma.post.create({
+      data: {
+        content: content,
+        title: title,
+      },
+    });
+
+    return fetchedCreatePost;
   } catch (error) {
     console.error("Failed to fetch chat sessions", error);
   }
--- src/app/components/InitializedMDXEditor/InitializedMDXEditor.tsx
+++ src/app/components/InitializedMDXEditor/InitializedMDXEditor.tsx
@@ -7,21 +7,21 @@ import {
   UndoRedo,
   toolbarPlugin,
 } from "@mdxeditor/editor";
-import { FC } from "react";
 
 interface EditorProps {
   markdown: string;
   editorRef?: React.MutableRefObject<MDXEditorMethods | null>;
+  onChange: (markdown: string) => void;
 }
 
 /**
  * Extend this Component further with the necessary plugins or props you need.
  * proxying the ref is necessary. Next.js dynamically imported components don't support refs.
  */
-const Editor: FC<EditorProps> = ({ markdown, editorRef }) => {
+const Editor = ({ markdown, editorRef, onChange }: EditorProps) => {
   return (
     <MDXEditor
-      onChange={(e) => console.log(e)}
+      onChange={(e) => onChange(e)}
       ref={editorRef}
       markdown={markdown}
       plugins={[
