0826c698b1d36f4741507e51f48f45514e47f52e
storatus <sebastian.glahn89@web.de>
2024-11-22 17:12:04 -0400

    fixed

--- src/app/components/BlogList/BlogListCard.tsx
+++ src/app/components/BlogList/BlogListCard.tsx
@@ -1,14 +1,18 @@
+"use client";
 import { ArrowRightIcon } from "lucide-react";
 // import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { Post } from "@prisma/client";
 
 export default function BlogListCard({ ...post }: Post) {
   const { content, title } = post;
+
+  const [isOptions, setIsOptions] = useState(false);
+
   return (
     <div className="h-full p-1">
-      <div className=" w-full max-w-xl bg-white shadow-md hover:scale-105 hover:shadow-lg break-words rounded-2xl   transition-transform duration-300 h-full">
+      <div className=" w-full max-w-xl bg-white shadow-md hover:shadow-lg break-words rounded-2xl   transition-transform duration-300 h-full">
         <div className="overflow-hidden rounded-t-2xl">
           {/* <Image
             width={500}
@@ -18,22 +22,43 @@ export default function BlogListCard({ ...post }: Post) {
             priority
             src={imageUrl}
             alt={title}
-            className=" w-full object-cover"
+            className="w-full object-cover"
           /> */}
         </div>
-        <div className="p-6">
-          {/* <div className="my-4 flex text-gray-400">
-            <h5 className="text-sm font-medium">{date}</h5>
-          </div> */}
-          <h3 className="mb-4 text-2xl font-bold text-gray-80">{title}</h3>
-          <p className="mb-6 line-clamp-3 text-gray-600">{content}</p>
-          <Link
-            href={`/blog/${title}`}
-            className="mt-4 flex items-center font-semibold text-yellow-500 transition-colors duration-200 hover:text-yellow-600"
-          >
-            Learn more
-            <ArrowRightIcon className="ml-2 text-sm" />
-          </Link>
+        <div className="p-2 flex flex-col h-full">
+          <div>
+            <div
+              onClick={() => {
+                setIsOptions(!isOptions);
+              }}
+              className=" cursor-pointer mb-2 flex items-center text-gray-400 hover:text-gray-600"
+            >
+              <span className="underline">Ajustar</span>
+            </div>
+
+            {isOptions && (
+              <div className="flex items-center justify-end  bg-white w-auto py-4">
+                <div className=" rounded border-red-400 h-8 w-20 border-2 justify-center items-center flex">
+                  <span className="text-red-400">Borrar</span>
+                </div>
+              </div>
+            )}
+          </div>
+          <div className="min-h-20">
+            <h3 className="text-2xl font-bold text-gray-80">{title}</h3>
+          </div>
+          <div className="flex-1 ">
+            <p className=" line-clamp-3 text-gray-600">{content}</p>
+          </div>
+          <div className="h-12 flex items-center justify-between px-2">
+            <Link
+              href={`/blog/${title}`}
+              className=" flex items-center font-semibold text-yellow-500 transition-colors duration-200 hover:text-yellow-600"
+            >
+              Ver mas
+              <ArrowRightIcon className="ml-2 text-sm" />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -4,19 +4,12 @@ export default function Footer() {
   return (
     <>
       <footer className="max-w-6xl container mx-auto flex min-h-10 items-center justify-between bg-black px-2">
-        <Link href={`/sobreMi`} className="flex w-1/5 items-center">
+        <Link href={`/`} className="flex w-1/5 items-center">
           <span className="  border-b border-opacity-70  text-xxs text-white text-opacity-90 hover:border-green-400 lg:p-1 lg:text-xs">
-            Sobre Mi
+            Home
           </span>
         </Link>
-        <div className="flex flex-1  items-center justify-evenly text-xxs leading-3 md:text-sm">
-          <Link
-            href={`/terms`}
-            className="text-opacity-8  justify-center hyphens-manual text-white hover:underline"
-          >
-            {"Terminos"}
-          </Link>
-        </div>
+
         <div className="flex w-1/5 items-center justify-end">
           <span className="text-xxs text-white text-opacity-80 md:text-sm">
             @ 2024 Mi Blog
